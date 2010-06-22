51fc4ff9dc4f69f312a6ed6fce0d1a7e90ef4a1f
Daniel Robbins <drobbins@funtoo.org>
2010-06-22 10:31:37 -0600

    merged tree

--- scripts/grabba.sh
+++ scripts/grabba.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dest=/var/tmp/git/portage-testmerge
+dest=/var/tmp/git/portage-gentoo
 src=`pwd`
 
 die() {
@@ -9,8 +9,20 @@ die() {
 }
 
 [ ! -d $dest ] && die "dest dir $dest does not exist"
+
+( cd $dest; git branch -D testmerge; )
+( cd $dest; git checkout gentoo.org; ) || die "couldn't checkout gentoo.org"
+( cd $dest; git checkout -b testmerge; ) || die "couldn't create testmerge"
+
 [ -e funtoo/funtoo-revert ] || die "funtoo-revert missing"
 [ -e funtoo/funtoo-misc ] || die "funtoo-misc missing"
+
+# Patches:
+for pat in `cat funtoo/patches/series | grep -v '^#'`
+do
+	( cd $dest; git apply "$src/funtoo/patches/$pat" ) || die "patch $pat failed"
+done
+
 
 for foo in `cat funtoo/funtoo-revert funtoo/funtoo-misc | grep -v '^#'`
 do
@@ -19,7 +31,7 @@ done
 
 # "*-*" will eliminate licenses, eclass, funtoo directories:
 
-for foo in `ls -d *-*/*`
+for foo in `ls -d *-*/* virtual/*`
 do
 	dirn="`dirname $foo`"
 	if [ ! -d $dest/$foo ]
@@ -32,12 +44,6 @@ do
 	fi
 done
 
-# Patches:
-for pat in `cat funtoo/patches/series | grep -v '^#'`
-do
-	( cd $dest; cat "$src/funtoo/patches/$pat" | patch -p1; ) || die "patch $pat failed"
-done
-
 # Misc files:
 
 cp -a sets.conf $dest/ || die "sets.conf fail"
@@ -46,3 +52,7 @@ rsync -a scripts/ $dest/scripts/ || die "rsync scripts fail"
 cp licenses/* $dest/licenses/ || die "licenses fail"
 cp eclass/* $dest/eclass/ || die "eclass fail"
 
+git add . || die "couldn't add"
+git commit -a -m "merged tree" || die "couldn't merge tree"
+
+tar cvf /var/tmp/git/curmerge.tar -C $dest --exclude .git
