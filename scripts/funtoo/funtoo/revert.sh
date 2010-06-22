345d02932e73d035e92ce3d0e85f1e86ab371137
Daniel Robbins <drobbins@funtoo.org>
2010-06-21 18:02:33 -0600

    cleanups

--- scripts/funtoo/funtoo/revert.sh
+++
@@ -1,9 +0,0 @@
-PORTDIR=/usr/portage
-cd $PORTDIR
-for x in `cat $PORTDIR/profiles/funtoo-revert | grep -v '^#'`
-do
-	[ ! -e "$x" ] && echo "$x does not exist. exiting." && exit 1
-	git rm -rf $x
-	git checkout origin/funtoo.org -- $x
-done
-
