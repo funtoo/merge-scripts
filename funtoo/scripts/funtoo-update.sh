c701040e7bffe37c6123cac22a26f4b29473e076
Daniel Robbins <drobbins@funtoo.org>
2010-06-24 06:56:43 -0600

    deprecating funtoo-misc and funtoo-revert

--- funtoo/scripts/funtoo-update.sh
+++ funtoo/scripts/funtoo-update.sh
@@ -39,34 +39,19 @@ die() {
 ( cd $dest; git branch -D testmerge; )
 ( cd $dest; git checkout -b testmerge; ) || die "couldn't create testmerge"
 
-[ -e funtoo/funtoo-revert ] || die "funtoo-revert missing"
-[ -e funtoo/funtoo-misc ] || die "funtoo-misc missing"
-
 # Patches:
 for pat in `cat funtoo/patches/series | grep -v '^#'`
 do
 	( cd $dest; git apply "$src/funtoo/patches/$pat" ) || die "patch $pat failed"
 done
 
-
-for foo in `cat funtoo/funtoo-revert funtoo/funtoo-misc | grep -v '^#'`
-do
-	( cd $dest; [ -e $foo ] && rm -rf $foo; )
-done
-
 # "*-*" will eliminate licenses, eclass, funtoo directories:
 
 for foo in `ls -d *-*/*`
 do
-	dirn="`dirname $foo`"
-	if [ ! -d $dest/$foo ]
-	then
-		install -d `dirname $dest/$foo` || die "install -d fail"
-		cp -a $foo $dest/$foo || die "cp -a fail"
-	else
-		echo "ERROR Already exists - $foo"
-		die "dir exists failure"
-	fi
+	[ -d $dest/$foo ] && echo "Replacing upstream ${foo}..." && ( cd $dest; [ -e $foo ] && rm -rf $foo; )
+	install -d `dirname $dest/$foo` || die "install -d fail"
+	cp -a $foo $dest/$foo || die "cp -a fail"
 done
 
 # Misc files:
