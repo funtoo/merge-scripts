475a0cc5ad14e36e333a73ae0d2c8f2e94f7f936
Daniel Robbins <drobbins@funtoo.org>
2010-12-14 00:43:19 -0700

    updated/improved merge.py that completed successfully - results need to be inspected and reviewed

--- funtoo/scripts/merge.py
+++ funtoo/scripts/merge.py
@@ -33,10 +33,12 @@ class ApplyPatchSeries(MergeStep):
 			runShell( "( cd %s; git apply %s/%s )" % ( tree.root, self.path, line[:-1] ))
 
 class SyncDir(MergeStep):
-	def __init__(self,srcroot,srcdir=None,destdir=None):
+	def __init__(self,srcroot,srcdir=None,destdir=None,exclude=[],delete=False):
 		self.srcroot = srcroot
 		self.srcdir = srcdir
 		self.destdir = destdir
+		self.exclude = exclude
+		self.delete = delete
 
 	def run(self,tree):
 		if self.srcdir:
@@ -50,8 +52,18 @@ class SyncDir(MergeStep):
 				dest = os.path.join(tree.root,self.srcdir)+"/"
 			else:
 				dest = os.path.normpath(tree.root)+"/"
-		cmd = "rsync -a --exclude /.git %s %s" % ( src, dest )
+		cmd = "rsync -a --exclude /.git "
+		for e in self.exclude:
+			cmd += "--exclude %s " % e
+		if self.delete:
+			cmd += "--delete "
+		cmd += "%s %s" % ( src, dest )
 		runShell(cmd)
+
+class SyncTree(SyncDir):
+	# sync a full portage tree, deleting any excess files in the target dir:
+	def __init__(self,srcroot,exclude=[]):
+		SyncDir.__init__(self,srcroot,srcdir=None,destdir=None,exclude=exclude,delete=True)
 
 class UnifiedTree(object):
 	def __init__(self,root,steps):
@@ -114,21 +126,71 @@ class InsertEbuilds(MergeStep):
 				else:
 					runShell("[ ! -e %s ] && cp -a %s %s || echo \"# skipping %s/%s\"" % (tpkgdir, pkgdir, tpkgdir, cat, pkg ))
 
+class ProfileDepFix(MergeStep):
+
+	def run(self,tree):
+		fpath = os.path.join(tree.root,"profiles/profiles.desc")
+		if os.path.exists(fpath):
+			a = open(fpath,"r")
+			for line in a:
+				if line[0:1] == "#":
+					continue
+				sp = line.split()
+				if len(sp) >= 2:
+					prof_path = sp[2]
+					runShell("rm -f %s/profiles/%s/deprecated" % ( tree.root, prof_path ))
+
+class GenCache(MergeStep):
+	def run(self,tree):
+		runShell("egencache --update --portdir=%s --jobs=12" % tree.root)
+
+class GitPrep(MergeStep):
+	def __init__(self,branch):
+		self.branch = branch
+
+	def run(self,tree):
+		runShell("( cd %s; git checkout %s )" % ( tree.root, self.branch ))
+
+class Minify(MergeStep):
+	def run(self,tree):
+		runShell("( cd %s; find -iname ChangeLog -exec rm -f {} \; )" % tree.root )
+		runShell("( cd %s; find -iname Manifest -exec sed -n -i -e \"/DIST/p\" {} \; )" % tree.root )
+
+class GitAdd(MergeStep):
+	def __init__(self,commit=False,message="(no message)"):
+		self.commit = commit
+		self.message = message
+	def run(self,tree):
+		runShell("( cd %s; git add . )" % tree.root )
+		if self.commit:
+			runShell("( cd %s; [ -n \"$(git status --porcelain)\" ] && git commit -a -m \"%s\" )" % ( tree.root, self.message ))
+
 steps = [
-	SyncDir("/var/git/portage-gentoo"),
+	SyncTree("/var/git/portage-gentoo",exclude=["/metadata/cache/**"]),
 	ApplyPatchSeries("/root/git/funtoo-overlay/funtoo/patches"),
 	SyncDir("/root/git/funtoo-overlay","funtoo/profiles","profiles"),
+	ProfileDepFix(),
 	SyncDir("/root/git/funtoo-overlay","licenses"),
 	SyncDir("/root/git/funtoo-overlay","eclass"),
 	InsertEbuilds("/root/git/funtoo-overlay",replace=True),
-	InsertEbuilds("/root/git/tarsius-overlay",replace=False)
+	InsertEbuilds("/root/git/tarsius-overlay",replace=False),
+	GenCache()
 ]
 
 
-"""
-ProfileFix(),
-InsertEbuilds("/root/git/tarsius-portage",replace=["sys-apps/foo"])
-"""
-
-a = UnifiedTree("/var/git/portage-test",steps)
+a = UnifiedTree("/var/src/merge-portage-work",steps)
 a.run()
+b = UnifiedTree("/var/git/merge-portage-prod", [ 
+	GitPrep("funtoo.org"), 
+	SyncTree("/var/src/merge-portage-work"), 
+	GitAdd(commit=True,message="glorious funtoo updates") 
+])
+b.run()
+c = UnifiedTree("/var/git/merge-portage-mini", [ 
+	GitPrep("funtoo.org"), 
+	SyncTree("/var/src/merge-portage-work", exclude=["ChangeLog"]), 
+	Minify(), 
+	GitAdd(commit=True,message="glorious funtoo updates") 
+])
+c.run()
+
