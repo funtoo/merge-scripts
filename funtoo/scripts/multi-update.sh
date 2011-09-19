e15277207f4293a8d5eaf1186883a328d3bc0fbd
Daniel Robbins <drobbins@funtoo.org>
2011-09-18 20:46:53 -0600

    update multi-update to use new syntax

--- funtoo/scripts/multi-update.sh
+++ funtoo/scripts/multi-update.sh
@@ -10,5 +10,5 @@ die() {
 cd /root/git/funtoo-overlay || die "couldn't chdir"
 git pull
 ./funtoo/scripts/gentoo-update.sh || die "gentoo update failed"
-./funtoo/scripts/merge.py || die "funtoo update failed"
+./funtoo/scripts/merge.py /var/git/portage-mini-2011 || die "funtoo update failed"
 rm -f /var/tmp/.regen_running
