e377b63ca23b1715c4e843ae0583efccf4993071
Daniel Robbins <drobbins@funtoo.org>
2011-12-19 20:10:31 -0700

    adding stage3.provided for package.provided bootstrap (reBootstrap)

--- funtoo/scripts/generate-system-tree.py
+++ funtoo/scripts/generate-system-tree.py
@@ -4,11 +4,16 @@ from merge_utils import *
 
 funtoo_src = Tree("funtoo","funtoo.org", "git://github.com/funtoo/portage-mini-2011.git", pull=True, trylocal="/var/git/portage-mini-2011")
 
+pf = open("stage3.txt","r")
+pkgs = []
+for line in pf:
+	pkgs.append(line[:-1])
+
 steps = [
 	CleanTree(),
 	SyncDir(funtoo_src.root,"eclass"),
 	SyncDir(funtoo_src.root,"profiles"),
-	InsertEbuilds(funtoo_src, select=["sys-apps/portage"])
+	InsertEbuilds(funtoo_src, select=pkgs)
 ]
 
 sys = UnifiedTree("/var/git/portage-system",steps)
