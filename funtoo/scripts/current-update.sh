dce108213008f75b49fa20bb5bb59270316e50f8
Daniel Robbins <drobbins@funtoo.org>
2012-01-28 11:31:38 -0700

    do git pull in main repo before regen.
    This ensures that we have the latest version of merge.py and friends, so that
    any changes in these files will be reflected in the merge.

--- funtoo/scripts/current-update.sh
+++ funtoo/scripts/current-update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 eval `keychain --noask --eval id_dsa`  || exit 1
-
+cd /root/git/funtoo-overlay
+# get latest merge.py and friends
+git pull || exit 1
 /root/git/funtoo-overlay/funtoo/scripts/merge.py --branch master /var/git/portage-mini-2011 /var/git/ports-2012 || exit 1
