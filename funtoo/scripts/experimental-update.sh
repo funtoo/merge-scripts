5f7c69d70b8084dea824fb885ebb8026be259404
Jean-Francis Roy <jeanfrancisroy@gmail.com>
2012-03-23 14:00:25 -0400

    Temporarily disabling the experimental branch: now experimental == master

--- funtoo/scripts/experimental-update.sh
+++ funtoo/scripts/experimental-update.sh
@@ -3,4 +3,5 @@ eval `keychain --noask --eval id_dsa`  || exit 1
 cd /root/git/funtoo-overlay
 # get latest merge.py and friends
 git pull || exit 1
-/root/git/funtoo-overlay/funtoo/scripts/merge.py --branch experimental /var/git/experimental-mini-2011 || exit 1
+#/root/git/funtoo-overlay/funtoo/scripts/merge.py --branch experimental /var/git/experimental-mini-2011 || exit 1
+/root/git/funtoo-overlay/funtoo/scripts/merge.py --branch master /var/git/experimental-mini-2011 || exit 1
