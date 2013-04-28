f17397f7a940b683656b8ef33a375aa2f91b0196
Daniel Robbins <drobbins@funtoo.org>
2013-04-28 00:55:01 +0000

    FL-482: mate mix-in, and eclass and package.mask integration

--- funtoo/scripts/merge.py
+++ funtoo/scripts/merge.py
@@ -43,6 +43,11 @@ steps = [
 	InsertEbuilds(bar_overlay, select="all", skip=["app-emulation/qemu"], replace=False),
 	InsertEbuilds(flora_overlay, select="all", skip=["sys-fs/spl", "sys-fs/zfs"], replace=False),
 	InsertEbuilds(mate_overlay, select="all", skip=None, replace=False),
+	SyncFiles(mate_overlay.root, "eclass"),
+	SyncFiles(mate_overlay.root, { 
+		"profiles/package.mask":"profiles/package.mask/mate",
+		"profiles/use.mask":"profiles/funtoo/1.0/linux-gnu/mix-ins/mate/use.mask"
+	}),
 	InsertEbuilds(sabayon_for_gentoo, select=["app-admin/equo", "app-admin/matter", "sys-apps/entropy", "sys-apps/entropy-server", "sys-apps/entropy-client-services","app-admin/rigo", "sys-apps/rigo-daemon", "sys-apps/magneto-core", "x11-misc/magneto-gtk", "x11-misc/magneto-gtk3", "kde-misc/magneto-kde", "app-misc/magneto-loader"], replace=True),
 	SyncDir(progress_overlay.root, "eclass"),
 	SyncDir(progress_overlay.root, "profiles/unpack_dependencies"),
