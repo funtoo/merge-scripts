ac21ee7dd689915277f3b958336b3b430976af8d
Oleg Vinichenko <oleg@funtoo.org>
2013-06-15 04:20:12 +0000

    merge fearedbliss overlay to tree

--- funtoo/scripts/merge.py
+++ funtoo/scripts/merge.py
@@ -15,6 +15,7 @@ flora_overlay = Tree("flora", "master", "repos@git.funtoo.org:flora.git", pull=T
 progress_overlay = SvnTree("progress", "https://gentoo-progress.googlecode.com/svn/overlays/progress")
 sabayon_for_gentoo = Tree("sabayon-for-gentoo", "master", "git://github.com/Sabayon/for-gentoo.git", pull=True)
 mate_overlay = Tree("mate", "master", "git://github.com/Sabayon/mate-overlay.git", pull=True)
+bliss_overlay = Tree("bliss", "master", "git://github.com/fearedbliss/bliss-overlay.git", pull=True)
 
 steps = [
 	SyncTree(gentoo_src,exclude=["/metadata/cache/**","ChangeLog", "dev-util/metro"]),
@@ -41,8 +42,9 @@ steps = [
 	InsertEbuilds(funtoo_overlay, select="all", skip=None, replace=True),
 	InsertEbuilds(foo_overlay, select="all", skip=["sys-fs/mdev-bb", "media-video/handbrake"], replace=["app-shells/rssh","net-misc/unison"]),
 	InsertEbuilds(bar_overlay, select="all", skip=["app-emulation/qemu"], replace=False),
-	InsertEbuilds(flora_overlay, select="all", skip=["sys-fs/spl", "sys-fs/zfs"], replace=False),
+	InsertEbuilds(flora_overlay, select="all", skip=["sys-fs/spl", "sys-fs/zfs", "media-sound/oss"], replace=False),
 	InsertEbuilds(mate_overlay, select="all", skip=None, replace=False),
+	InsertEbuilds(bliss_overlay, select="all", skip=None, replace=False),
 	SyncDir(mate_overlay.root, "eclass"),
 	SyncDir(mate_overlay.root, "sets"),
 	SyncFiles(mate_overlay.root, { 
