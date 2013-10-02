93c093d67401b05163a8c498904b45e961f06715
Oleg Vinichenko <oleg@funtoo.org>
2013-10-02 01:22:18 +0000

    FL-808: openntpd unfork

--- net-misc/openntpd/files/openntpd-3.9_p1-r4/ntpd.conf.d
+++
@@ -1,7 +0,0 @@
-# /etc/conf.d/ntpd: config file for openntpd's ntpd
-
-NTPD_HOME=/var/lib/openntpd/chroot
-
-# See ntpd(8) man page ... some popular options:
-#  -s   Set the time immediately at startup
-NTPD_OPTS=""
