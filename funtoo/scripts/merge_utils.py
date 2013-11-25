fc06cc512a6d2f0030917b01ed9560716f560216
Daniel Robbins <drobbins@funtoo.org>
2013-11-25 11:24:52 -0700

    FL-899: create new directories with cvs, prune old ones

--- funtoo/scripts/merge_utils.py
+++ funtoo/scripts/merge_utils.py
@@ -268,7 +268,7 @@ class CvsTree(object):
 		if not os.path.exists(base):
 			os.makedirs(base)
 		if os.path.exists(self.root):
-			runShell("(cd %s; cvs --no-verify update)" % self.root)
+			runShell("(cd %s; cvs --no-verify update -dP)" % self.root)
 		else:
 			runShell("(cd %s; cvs --no-verify -d %s co %s)" % (base, self.url, self.name))
 
