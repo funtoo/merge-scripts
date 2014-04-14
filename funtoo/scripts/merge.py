0908e6b0b00a2d1da12ee78bd1a0373b1bc2e04d
Daniel Robbins <drobbins@funtoo.org>
2014-04-13 19:40:17 -0600

    FL-1181: adding ebuilds, eclasses, masks

--- funtoo/scripts/merge.py
+++ funtoo/scripts/merge.py
@@ -43,6 +43,7 @@ steps = [
 		"profiles/arch/amd64/no-multilib/use.mask":"profiles/funtoo/1.0/linux-gnu/arch/pure64/use.mask/01-gentoo"
 	}),
 	InsertEbuilds(funtoo_overlay, select="all", skip=None, replace=True),
+        InsertEbuilds(mysql_overlay, select="all", skip=None, replace=True),
 	InsertEbuilds(foo_overlay, select="all", skip=["sys-fs/mdev-bb", "sys-fs/mdev-like-a-boss", "media-video/handbrake"], replace=["app-shells/rssh","net-misc/unison"]),
 	InsertEbuilds(bar_overlay, select="all", skip=["app-emulation/qemu"], replace=False),
         InsertEbuilds(bliss_overlay, select="all", skip=None, replace=False),
@@ -52,6 +53,11 @@ steps = [
 	SyncFiles(funtoo_gnome_overlay.root, {
 		"profiles/package.mask/funtoo-gnome3.8":"profiles/funtoo/1.0/linux-gnu/mix-ins/gnome/package.mask/01-gnome"
 	}),
+        SyncFiles(mysql_overlay.root, {
+                "profiles/package.mask":"profiles/package.mask/mysql",
+                "profiles/package.use.mask":"profiles/package.use.mask/mysql"
+        }),
+        SyncDir(mysql_overlay.root, "eclass"),
 	SyncDir(mate_overlay.root, "eclass"),
 	SyncDir(mate_overlay.root, "sets"),
 	SyncFiles(mate_overlay.root, { 
