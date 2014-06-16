2ba1101375cc7542d19f54437ec960abe11a6c31
Daniel Robbins <drobbins@funtoo.org>
2014-06-16 03:01:01 +0000

    FL-624: rust is now in gentoo. unforking.

--- funtoo/scripts/gentoo-compare.sh
+++ funtoo/scripts/gentoo-compare.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-eval `keychain --noask --eval id_dsa` || exit 1
-/root/git/funtoo-overlay/funtoo/scripts/gentoo-compare-json.py /var/cvs/source-trees/gentoo-x86
+/root/funtoo-overlay/funtoo/scripts/gentoo-compare-json.py /var/rsync/source-trees/gentoo
