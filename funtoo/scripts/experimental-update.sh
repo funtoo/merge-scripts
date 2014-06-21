ae5a4df07819cadeb7132bd699cb8dc3fd2cf542
Daniel Robbins <drobbins@funtoo.org>
2014-06-21 20:14:20 +0000

    experimental-update: fix paths

--- funtoo/scripts/experimental-update.sh
+++ funtoo/scripts/experimental-update.sh
@@ -2,4 +2,4 @@
 cd /root/funtoo-overlay
 # get latest merge.py and friends
 git pull || exit 1
-/root/git/funtoo-overlay/funtoo/scripts/merge.py --branch master /var/git/experimental-2014 || exit 1
+/root/funtoo-overlay/funtoo/scripts/merge.py --branch master /var/git/experimental-2014 || exit 1
