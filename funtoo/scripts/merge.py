ee90c0615e13b8f9a06fba87375107e57adb373b
Dantrell B <dantrell@mirthil.org>
2014-07-30 00:32:20 -0400

    Add missing comma to funtoo/scripts/merge.py

--- funtoo/scripts/merge.py
+++ funtoo/scripts/merge.py
@@ -73,7 +73,7 @@ steps = [
                 "profiles/package.mask":"profiles/package.mask/deadbeef-mask"
         }),
 	SyncFiles(funtoo_gnome_overlay.root, {
-		"profiles/package.mask":"profiles/funtoo/1.0/linux-gnu/mix-ins/gnome/package.mask"
+		"profiles/package.mask":"profiles/funtoo/1.0/linux-gnu/mix-ins/gnome/package.mask",
 		"profiles/package.unmask":"profiles/funtoo/1.0/linux-gnu/mix-ins/gnome/package.unmask"
 	}),
         SyncFiles(mysql_overlay.root, {
