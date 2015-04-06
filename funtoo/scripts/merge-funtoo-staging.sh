0ea6ada65e416f132fc409acdb1f1174cb6937bc
Daniel Robbins <drobbins@funtoo.org>
2015-04-06 18:51:01 +0000

    zero exit codes for frequently-run cron job

--- funtoo/scripts/merge-funtoo-staging.sh
+++ funtoo/scripts/merge-funtoo-staging.sh
@@ -2,6 +2,7 @@
 cd /root/funtoo-overlay
 # get latest merge.py and friends
 git pull || exit 1
-/root/funtoo-overlay/funtoo/scripts/merge-funtoo-staging.py || exit 1
+/root/funtoo-overlay/funtoo/scripts/merge-funtoo-staging.py || exit 0
 # This will only run if previous script returned zero, which only happens on a real regen...
-/root/funtoo-overlay/funtoo/scripts/merge-funtoo-production.py || exit 1
+/root/funtoo-overlay/funtoo/scripts/merge-funtoo-production.py || exit 0
+exit 0
