9f4d5cedb8e01a07ad5fa7817d833ad5b619e76f
Daniel Robbins <drobbins@funtoo.org>
2015-04-06 15:09:48 +0000

    Add ability to have multiple QA builds.

--- funtoo/scripts/merge-funtoo-production.py
+++ funtoo/scripts/merge-funtoo-production.py
@@ -4,15 +4,28 @@ import os
 import subprocess
 from merge_utils import *
 
+host="root@build.funtoo.org"
+arch_desc="x86-64bit"
+subarch="corei7"
+
+def qa_build(host,build,arch_desc,subarch,head):
+	success = False
+	exists = subprocess.getoutput("ssh %s '[ -e /home/mirror/funtoo/%s/%s/%s/" % ( host, build, arch_desc, subarch ) + head + "/status ] && echo yep || echo nope'") == "yep"
+	if not exists:
+		status, output = subprocess.getstatusoutput("ssh %s /root/metro/scripts/ezbuild.sh %s %s %s freshen " % ( build, arch_desc, subarch ) + head)
+	success = subprocess.getoutput("ssh %s cat /home/mirror/funtoo/%s/%s/%s/" % ( host, build, arch_desc, subarch )  + head + "/status") == "ok"
+	return success
+
 funtoo_staging_r = GitTree("funtoo-staging", "master", "repos@git.funtoo.org:ports/funtoo-staging.git", pull=True)
 head = funtoo_staging_r.head()
 print(head)
-exists = subprocess.getoutput("ssh root@build.funtoo.org '[ -e /home/mirror/funtoo/funtoo-current-next/x86-64bit/corei7/" + head + "/status ] && echo yep || echo nope'") == "yep"
-if not exists:
-	status, output = subprocess.getstatusoutput("ssh root@build.funtoo.org /root/metro/scripts/ezbuild.sh funtoo-current-next x86-64bit corei7 freshen " + head)
-success = subprocess.getoutput("ssh root@build.funtoo.org cat /home/mirror/funtoo/funtoo-current-next/x86-64bit/corei7/" + head + "/status") == "ok"
+success = False
+if qa_build(host,"funtoo-current-next",arch_desc,subarch,head):
+	if qa_build(host,"funtoo-stable-next",arch_desc,subarch,head):
+		success = True
 if not success:
-	print("QA build did not complete successfully. Exiting.")
+	print("QA builds were not successful.")
+	sys.exit(1)
 
 ports_2012 = GitTree("ports-2012", "funtoo.org", "repos@git.funtoo.org:ports-2012.git", root="/var/git/dest-trees/ports-2012", pull=False)
 
