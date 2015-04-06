9ef3d8111c0422e4b3437ef893958c22274f6beb
Daniel Robbins <drobbins@funtoo.org>
2015-04-06 07:44:02 +0000

    Latest improvements for funtoo merge process.

--- funtoo/scripts/merge-funtoo-production.py
+++ funtoo/scripts/merge-funtoo-production.py
@@ -4,22 +4,16 @@ import os
 import subprocess
 from merge_utils import *
 
-def run(cmdargs, env={}):
-	print("Running command: %s (env %s) " % ( cmdargs,env ))
-	try:
-		cmd = subprocess.Popen(cmdargs, env=env)
-		exitcode = cmd.wait()
-	except KeyboardInterrupt:
-		cmd.terminate()
-		print("Interrupted via keyboard!")
-		return 1
-	else:
-		if exitcode != 0:
-			print("Command exited with return code %s" % exitcode)
-			return exitcode
-		return 0
+funtoo_staging_r = GitTree("funtoo-staging", "master", "repos@git.funtoo.org:ports/funtoo-staging.git", pull=True)
+head = funtoo_staging_r.head()
+print(head)
+exists = subprocess.getoutput("ssh root@build.funtoo.org '[ -e /home/mirror/funtoo/funtoo-current-next/x86-64bit/corei7/" + head + "/status ] && echo yep || echo nope'") == "yep"
+if not exists:
+	status, output = subprocess.getstatusoutput("ssh root@build.funtoo.org /root/metro/scripts/ezbuild.sh funtoo-current-next x86-64bit corei7 freshen " + head)
+success = subprocess.getoutput("ssh root@build.funtoo.org cat /home/mirror/funtoo/funtoo-current-next/x86-64bit/corei7/" + head + "/status") == "ok"
+if not success:
+	print("QA build did not complete successfully. Exiting.")
 
-funtoo_staging_r = GitTree("funtoo-staging", "master", "repos@git.funtoo.org:ports/funtoo-staging.git", commit=commit, pull=True)
 ports_2012 = GitTree("ports-2012", "funtoo.org", "repos@git.funtoo.org:ports-2012.git", root="/var/git/dest-trees/ports-2012", pull=False)
 
 my_steps = [
@@ -28,7 +22,7 @@ my_steps = [
 	GenCache()
 ]
 
-retval = run(["/usr/bin/ssh","root@et.host.funtoo.org","/root/metro/scripts/ezbuild.sh","funtoo-current-next","x86-64bit","corei7",funtoo_staging_r.head(), os.environ)
-
 ports_2012.run(my_steps)
-ports_2012.gitCommit(message="merged from funtoo-staging", branch="funtoo.org")
+#ports_2012.gitCommit(message="merged from funtoo-staging", branch="funtoo.org")
+ports_2012.gitCommit(message="merged from funtoo-staging", branch=False)
+
