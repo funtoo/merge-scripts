c1b8c3c8552c72e6c27a42d2a420d582d4baeb7d
Daniel Robbins <drobbins@funtoo.org>
2015-05-01 10:28:13 -0600

    Updating QA build code slightly.

--- funtoo/scripts/merge-funtoo-production.py
+++ funtoo/scripts/merge-funtoo-production.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
 
-import os
-import subprocess
 from merge_utils import *
 
 host="root@build.host.funtoo.org"
 arch_desc="x86-64bit"
 subarch="corei7"
-
-def qa_build(host,build,arch_desc,subarch,head,target):
-	success = False
-	exists = subprocess.getoutput("ssh %s '[ -e /home/mirror/funtoo/%s/%s/%s/" % ( host, build, arch_desc, subarch ) + head + "/status ] && echo yep || echo nope'") == "yep"
-	if not exists:
-		status, output = subprocess.getstatusoutput("ssh %s /root/metro/scripts/ezbuild.sh %s %s %s %s " % ( host, build, arch_desc, subarch, target ) + head)
-	success = subprocess.getoutput("ssh %s cat /home/mirror/funtoo/%s/%s/%s/" % ( host, build, arch_desc, subarch )  + head + "/status") == "ok"
-	return success
 
 funtoo_staging_r = GitTree("funtoo-staging", "master", "repos@localhost:ports/funtoo-staging.git", pull=True)
 head = funtoo_staging_r.head()
@@ -22,7 +12,6 @@ print(head)
 success = False
 if qa_build(host,"funtoo-current-next",arch_desc,subarch,head,"freshen"):
 	if qa_build(host,"funtoo-stable-next",arch_desc,subarch,head,"freshen"):
-#		if qa_build(host,"funtoo-current-gnome-next",arch_desc,subarch,head,"test"):
 		success = True
 if not success:
 	print("QA builds were not successful.")
