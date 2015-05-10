0b61eda518ccf1ab53cc2a6da6ba374a0de2c313
Daniel Robbins <drobbins@funtoo.org>
2015-05-10 02:16:21 +0000

    New master cron script.

--- funtoo/scripts/merge-funtoo.sh
+++ funtoo/scripts/merge-funtoo.sh
@@ -3,10 +3,11 @@ cd /root/funtoo-overlay
 # get latest merge.py and friends
 git pull || exit 1
 /root/funtoo-overlay/funtoo/scripts/merge-funtoo-staging.py || exit 0
-# This will only run if previous script returned zero, which only happens on a real regen...
 /root/funtoo-overlay/funtoo/scripts/merge-funtoo-production.py
 if [ $? -eq 0 ]; then 
 	# update comparison JSON
 	/root/funtoo-overlay/funtoo/scripts/gentoo-compare-json.py /var/git/dest-trees/ports-2012 /var/git/source-trees/gentoo-staging/
+	exit 0
+else
+	exit 1 
 fi
-exit $?
