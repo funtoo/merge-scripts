bd7821a25b28a184bb76f3f88ed0b0da94aa77fd
Daniel Robbins <drobbins@funtoo.org>
2015-05-10 03:32:29 +0000

    some debug statements for merge scripts.

--- funtoo/scripts/merge-funtoo.sh
+++ funtoo/scripts/merge-funtoo.sh
@@ -10,8 +10,11 @@ elif [ $? -ne 0 ]; then
 	# some kind of error
 	exit 1	
 fi
+echo "Starting merge-funtoo-production.py..."
 /root/funtoo-overlay/funtoo/scripts/merge-funtoo-production.py
-if [ $? -eq 0 ]; then 
+ecode=$?
+echo "merge-funtoo-production exit code: $ecode"
+if [ $ecode -eq 0 ]; then 
 	# update comparison JSON
 	/root/funtoo-overlay/funtoo/scripts/gentoo-compare-json.py /var/git/dest-trees/ports-2012 /var/git/source-trees/gentoo-staging/
 	exit 0
