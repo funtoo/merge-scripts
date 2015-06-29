a8e1f84a362c00e6531c7eee7194758df17abb05
Daniel Robbins <drobbins@funtoo.org>
2015-06-29 16:15:41 +0000

    Disable QA build for KDE, update to intel64-westmere stage3

--- funtoo/scripts/qa-builds.py
+++ funtoo/scripts/qa-builds.py
@@ -4,12 +4,12 @@ from merge_utils import *
 
 host="root@build.host.funtoo.org"
 arch_desc="x86-64bit"
-subarch="corei7"
+subarch="intel64-westmere"
 
 funtoo_staging_r = GitTree("funtoo-staging", "master", "repos@localhost:ports/funtoo-staging.git", pull=True)
 head = funtoo_staging_r.head()
 
 qa_build(host,"funtoo-current-xfce-next",arch_desc,subarch,head,"test")
-qa_build(host,"funtoo-current-kde-next",arch_desc,subarch,head,"test")
+#qa_build(host,"funtoo-current-kde-next",arch_desc,subarch,head,"test")
 qa_build(host,"funtoo-current-gnome-next",arch_desc,subarch,head,"test")
 
