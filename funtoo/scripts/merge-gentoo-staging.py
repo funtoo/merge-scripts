cef72871450547aa9a830c7c86c9d10f7ea4161d
Daniel Robbins <drobbins@funtoo.org>
2015-07-16 22:34:19 +0000

    FL-2624: fixing GLSAs -- Gentoo changed to a git repo.

--- funtoo/scripts/merge-gentoo-staging.py
+++ funtoo/scripts/merge-gentoo-staging.py
@@ -13,14 +13,14 @@ def gentoo_staging_update():
 		gentoo_src = RsyncTree("gentoo")
 	else:
 		gentoo_src = CvsTree("gentoo-x86",":pserver:anonymous@anoncvs.gentoo.org:/var/cvsroot")
-		gentoo_glsa = CvsTree("gentoo-glsa",":pserver:anonymous@anoncvs.gentoo.org:/var/cvsroot", path="gentoo/xml/htdocs/security")
+		gentoo_glsa = GitTree("gentoo-glsa", "master", "git://anongit.gentoo.org/data/glsa.git", pull=True)
 	# This is the gentoo-staging tree, stored in a different place locally, so we can simultaneously be updating gentoo-staging and reading
 	# from it without overwriting ourselves:
 	all_steps = [
 		GitCheckout("master"),
 		SyncFromTree(gentoo_src, exclude=["/metadata/cache/**", "ChangeLog", "dev-util/metro"]),
 		# Only include 2012 and up GLSA's:
-		SyncDir(gentoo_glsa.root, "en/glsa", "metadata/glsa", exclude=["glsa-200*.xml","glsa-2010*.xml", "glsa-2011*.xml"]) if not gentoo_use_rsync else None,
+		SyncDir(gentoo_glsa.root, srcdir=None, destdir="metadata/glsa", exclude=["glsa-200*.xml","glsa-2010*.xml", "glsa-2011*.xml"]) if not gentoo_use_rsync else None,
 	]
 	gentoo_staging_w.run(all_steps)
 	gentoo_staging_w.gitCommit(message="gentoo updates", branch="master")
