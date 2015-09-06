e2851a5fb2d00aeb89f389caaf932086cfa89681
Daniel Robbins <drobbins@funtoo.org>
2015-09-05 23:25:21 -0600

    Use intel64-westmere rather than deprecated corei7 for QA builds

--- funtoo/scripts/merge-funtoo-production.py
+++ funtoo/scripts/merge-funtoo-production.py
@@ -4,7 +4,7 @@ from merge_utils import *
 
 host="root@build.funtoo.org"
 arch_desc="x86-64bit"
-subarch="corei7"
+subarch="intel64-westmere"
 
 funtoo_staging_r = GitTree("funtoo-staging", "master", "repos@localhost:ports/funtoo-staging.git", pull=True)
 head = funtoo_staging_r.head()
