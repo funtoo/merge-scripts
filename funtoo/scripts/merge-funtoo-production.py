a7f4eb8d82601f4dc26fde7ccbcac4239e3c178c
Daniel Robbins <drobbins@funtoo.org>
2015-09-07 15:54:56 +0000

    Temporarily disabling QA builds as we update stages. Add KDE shard.

--- funtoo/scripts/merge-funtoo-production.py
+++ funtoo/scripts/merge-funtoo-production.py
@@ -4,16 +4,16 @@ from merge_utils import *
 
 host="root@build.funtoo.org"
 arch_desc="x86-64bit"
-subarch="intel64-westmere"
+subarch="generic_64"
 
 funtoo_staging_r = GitTree("funtoo-staging", "master", "repos@localhost:ports/funtoo-staging.git", pull=True)
 head = funtoo_staging_r.head()
 print(head)
-success = False
-if qa_build(host,"funtoo-current-next",arch_desc,subarch,head,"freshen"):
-	if qa_build(host,"funtoo-stable-next",arch_desc,subarch,head,"freshen"):
-		if qa_build(host,"funtoo-current-hardened",arch_desc,subarch,head,"freshen"):
-			success = True
+success = True
+#if qa_build(host,"funtoo-current-next",arch_desc,subarch,head,"freshen"):
+#	if qa_build(host,"funtoo-stable-next",arch_desc,subarch,head,"freshen"):
+#		if qa_build(host,"funtoo-current-hardened",arch_desc,subarch,head,"freshen"):
+#			success = True
 if not success:
 	print("QA builds were not successful.")
 	sys.exit(1)
--- funtoo/scripts/merge-gentoo-staging.py
+++ funtoo/scripts/merge-gentoo-staging.py
@@ -10,6 +10,7 @@ gentoo_staging_w = GitTree("gentoo-staging", "master", "repos@localhost:ports/ge
 
 perl_shard = GitTree("gentoo-perl-shard", "master", "repos@localhost:gentoo-perl-shard.git", root="/var/git/dest-trees/gentoo-perl-shard", pull=False)
 python_shard = GitTree("gentoo-python-shard", "master", "repos@localhost:gentoo-python-shard.git", root="/var/git/dest-trees/gentoo-python-shard", pull=False)
+kde_shard = GitTree("gentoo-kde-shard", "master", "repos@localhost:gentoo-kde-shard.git", root="/var/git/dest-trees/gentoo-kde-shard", pull=False)
 
 # This function updates the gentoo-staging tree with all the latest gentoo updates:
 
@@ -49,12 +50,27 @@ def gentoo_staging_update():
 		InsertEclasses(gentoo_staging_w, select=re.compile("gnome-python.*\.eclass")),
 		
 	]
+	kde_shard_steps = [
+		GitCheckout("master"),
+		CleanTree(),
+		InsertEbuilds(gentoo_staging_w, select=re.compile("dev-qt/.*"), skip=None, replace=True),
+		InsertEbuilds(gentoo_staging_w, select=re.compile("kde-apps/.*"), skip=None, replace=True),
+		InsertEbuilds(gentoo_staging_w, select=re.compile("kde-base/.*"), skip=None, replace=True),
+		InsertEbuilds(gentoo_staging_w, select=re.compile("kde-frameworks/.*"), skip=None, replace=True),
+		InsertEbuilds(gentoo_staging_w, select=re.compile("kde-misc/.*"), skip=None, replace=True),
+		InsertEbuilds(gentoo_staging_w, select=re.compile("kde-plasma/.*"), skip=None, replace=True),
+		InsertEclasses(gentoo_staging_w, select=re.compile("kde.*\.eclass")),
+		InsertEclasses(gentoo_staging_w, select=re.compile("qt4-.*\.eclass")),
+		InsertEclasses(gentoo_staging_w, select=re.compile("qt5-.*\.eclass")),
+	]
 	gentoo_staging_w.run(all_steps)
 	gentoo_staging_w.gitCommit(message="gentoo updates", branch="master")
 	perl_shard.run(perl_shard_steps)
 	perl_shard.gitCommit(message="gentoo updates", branch="master")
 	python_shard.run(python_shard_steps)
 	python_shard.gitCommit(message="gentoo updates", branch="master")
+	kde_shard.run(kde_shard_steps)
+	kde_shard.gitCommit(message="gentoo updates", branch="master")
 
 gentoo_staging_update()
 
