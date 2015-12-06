f1eb052f02318b675a255b47123a47d3af083859
Oleg Vinichenko <oleg@funtoo.org>
2015-12-06 06:06:33 +0000

    update x11-shard after adding media-sound/*

--- funtoo/scripts/merge-funtoo-staging.py
+++ funtoo/scripts/merge-funtoo-staging.py
@@ -35,14 +35,14 @@ shards = {
 	"perl" : GitTree("gentoo-perl-shard", "6b927ddbe7290c8898efa680c6b9ef39c591bfe3", "repos@localhost:gentoo-perl-shard.git", pull=True),
 	"kde" : GitTree("gentoo-kde-shard", "7462b19d11752911a3c66d8db3d98c5161f2a7ad", "repos@localhost:gentoo-kde-shard.git", pull=True),
 	"gnome" : GitTree("gentoo-gnome-shard", "4d5473019d599229cb54edde7f5a7e48df46302f", "repos@localhost:ports/gentoo-gnome-shard.git", pull=True),
-	"x11" : GitTree("gentoo-x11-shard", "862f29c57e7d7fb8f9d769836e525cffa7544a79", "repos@localhost:ports/gentoo-x11-shard.git", pull=True),
+	"x11" : GitTree("gentoo-x11-shard", "46a868af9e2b658fc7653caac8368484b8fef567", "repos@localhost:ports/gentoo-x11-shard.git", pull=True),
 	"core" : GitTree("gentoo-core-shard", "b347a4b6c1adfb905ea4bece903d710be5520fe3", "repos@localhost:gentoo-core-shard.git", pull=True)
 }
 
 # perl: 6b927ddbe7290c8898efa680c6b9ef39c591bfe3 (Updated 12 Nov 2015)
 # kde: 	7462b19d11752911a3c66d8db3d98c5161f2a7ad (Updated 30 Nov 2015)
 # gnome: 4d5473019d599229cb54edde7f5a7e48df46302f (Updated 5 Oct 2015)
-# x11: 	862f29c57e7d7fb8f9d769836e525cffa7544a79 (Updated 5 Dec 2015)
+# x11: 46a868af9e2b658fc7653caac8368484b8fef567 (Updated 6 Dec 2015)
 # core: b347a4b6c1adfb905ea4bece903d710be5520fe3 (Updated 12 Nov 2015)
 
 funtoo_overlays = {
