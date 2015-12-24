d4d249abc43a87d970874d2369a66cd37ff55267
Oleg Vinichenko <oleg@funtoo.org>
2015-12-24 13:24:21 +0000

    use build.funtoo.org/distfiles a first entry in thirdpartymirrors

--- funtoo/scripts/merge_utils.py
+++ funtoo/scripts/merge_utils.py
@@ -166,7 +166,7 @@ class ThirdPartyMirrors(MergeStep):
 	def run(self,tree):
 		orig = "%s/profiles/thirdpartymirrors" % tree.root
 		new = "%s/profiles/thirdpartymirrors.new" % tree.root
-		mirrors = "http://ftp.osuosl.org/pub/funtoo/distfiles http://build.funtoo.org/distfiles"
+		mirrors = "http://build.funtoo.org/distfiles http://ftp.osuosl.org/pub/funtoo/distfiles"
 		a = open(orig, "r")
 		b = open(new, "w")
 		for line in a:
