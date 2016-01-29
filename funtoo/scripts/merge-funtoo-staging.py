575fd27ec40689ffe9146947a017492386481a34
Oleg Vinichenko <oleg@funtoo.org>
2016-01-29 04:38:07 +0000

    update core shard

--- funtoo/scripts/merge-funtoo-staging.py
+++ funtoo/scripts/merge-funtoo-staging.py
@@ -36,14 +36,14 @@ shards = {
 	"kde" : GitTree("gentoo-kde-shard", "4f29f65bbe8beff980a8a68a90a492e46dabb90c", "repos@localhost:gentoo-kde-shard.git", pull=True),
 	"gnome" : GitTree("gentoo-gnome-shard", "4d5473019d599229cb54edde7f5a7e48df46302f", "repos@localhost:ports/gentoo-gnome-shard.git", pull=True),
 	"x11" : GitTree("gentoo-x11-shard", "c3506ae919008fe26c527afea12b1e600535795a", "repos@localhost:ports/gentoo-x11-shard.git", pull=True),
-	"core" : GitTree("gentoo-core-shard", "a00c829459e0a0237bd5116dcc5f46d125249932", "repos@localhost:gentoo-core-shard.git", pull=True)
+	"core" : GitTree("gentoo-core-shard", "9be16c57074e8c61f593601e2655bd4f80d69d3b", "repos@localhost:gentoo-core-shard.git", pull=True)
 }
 
 # perl: 4521e3d0637a0001337fd9b6730bf4d861174364 (Updated 27 Jan 2016)
 # kde: 4f29f65bbe8beff980a8a68a90a492e46dabb90c (Updated 25 Jan 2016)
 # gnome: 4d5473019d599229cb54edde7f5a7e48df46302f (Updated 5 Oct 2015)
 # x11: c3506ae919008fe26c527afea12b1e600535795a (Updated 18 Jan 2016)
-# core:	a00c829459e0a0237bd5116dcc5f46d125249932 (Updated 23 Jan 2016)
+# core:	9be16c57074e8c61f593601e2655bd4f80d69d3b (Updated 29 Jan 2016)
 # funtoo-toolchain: 4d2683c98ee27aa693bcff6ac846622c3bbdf2e2 (Updated 11 Jan 2016)
 
 funtoo_overlays = {
