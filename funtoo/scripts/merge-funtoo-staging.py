07fdab234c6966548167e489dd48aa16fb9fb773
Oleg Vinichenko <oleg@funtoo.org>
2016-05-03 04:41:42 +0000

    update x11 and kde shards

--- funtoo/scripts/merge-funtoo-staging.py
+++ funtoo/scripts/merge-funtoo-staging.py
@@ -33,17 +33,17 @@ gentoo_staging_r = GitTree("gentoo-staging", "master", "repos@localhost:ports/ge
 
 shards = {
 	"perl" : GitTree("gentoo-perl-shard", "ae43c0ed75dc2a197dda239be98be953c26bfba5", "repos@localhost:gentoo-perl-shard.git", pull=True),
-	"kde" : GitTree("gentoo-kde-shard", "ba1110afed403d7e16c6ea3b4db8e3a814d972e6", "repos@localhost:gentoo-kde-shard.git", pull=True),
+	"kde" : GitTree("gentoo-kde-shard", "efb36de599fa5709c0404f3793e5e02cb1553ddb", "repos@localhost:gentoo-kde-shard.git", pull=True),
 	"gnome" : GitTree("gentoo-gnome-shard", "4d5473019d599229cb54edde7f5a7e48df46302f", "repos@localhost:ports/gentoo-gnome-shard.git", pull=True),
-	"x11" : GitTree("gentoo-x11-shard", "76ff1c8de8dcf96e69d0706c8b6c2b73810cb5bc", "repos@localhost:ports/gentoo-x11-shard.git", pull=True),
+	"x11" : GitTree("gentoo-x11-shard", "0c65fb2de65c943178b5e2d64524f64f489039a2", "repos@localhost:ports/gentoo-x11-shard.git", pull=True),
 	"office" : GitTree("gentoo-office-shard", "3d80b90b8dba879735f9fe95ac5c45620792da3e", "repos@localhost:ports/gentoo-office-shard.git", pull=True),
 	"core" : GitTree("gentoo-core-shard", "af2561ccb3b736b1549b013b14788f3b1e112947", "repos@localhost:gentoo-core-shard.git", pull=True)
 }
 
 # perl: ae43c0ed75dc2a197dda239be98be953c26bfba5 (Updated 28 Apr 2016)
-# kde: ba1110afed403d7e16c6ea3b4db8e3a814d972e6 (Updated 28 Apr 2016)
+# kde: efb36de599fa5709c0404f3793e5e02cb1553ddb (Updated 3 May 2016)
 # gnome: 4d5473019d599229cb54edde7f5a7e48df46302f (Updated 5 Oct 2015)
-# x11: 76ff1c8de8dcf96e69d0706c8b6c2b73810cb5bc (Updated 28 Apr 2016)
+# x11: 0c65fb2de65c943178b5e2d64524f64f489039a2 (Updated 3 May 2016)
 # office: 3d80b90b8dba879735f9fe95ac5c45620792da3e (Updated 18 Apr 2016)
 # core:	af2561ccb3b736b1549b013b14788f3b1e112947 (Updated 2 May 2016)
 # funtoo-toolchain: 5d438b0aca7ca7ea2e074b38614e4997651729b4 (Updated 12 Apr 2016)
