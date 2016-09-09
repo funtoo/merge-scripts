1522120325d5dd46ce38eee89e43b59437b9f946
Oleg Vinichenko <oleg@funtoo.org>
2016-09-09 02:41:10 +0000

    FL-2284: add chuse from pinsard

--- funtoo/scripts/merge-funtoo-staging.py
+++ funtoo/scripts/merge-funtoo-staging.py
@@ -179,7 +179,7 @@ ebuild_modifications = [
 	InsertEbuilds(other_overlays["causes_overlay"], select=[ "media-sound/renoise", "media-sound/renoise-demo", "sys-fs/smdev", "x11-wm/dwm" ], skip=None, replace=True),
 	InsertEbuilds(other_overlays["sabayon_for_gentoo"], select=["app-admin/equo", "app-admin/matter", "sys-apps/entropy", "sys-apps/entropy-server", "sys-apps/entropy-client-services","app-admin/rigo", "sys-apps/rigo-daemon", "sys-apps/magneto-core", "x11-misc/magneto-gtk", "x11-misc/magneto-gtk3", "x11-themes/numix-icon-theme", "kde-misc/magneto-kde", "app-misc/magneto-loader", "media-video/kazam" ], replace=True),
 	InsertEbuilds(other_overlays["tripsix_overlay"], select=["media-sound/rakarrack"], skip=None, replace=True, merge=False),
-	InsertEbuilds(other_overlays["pinsard_overlay"], select=["x11-wm/qtile"], skip=None, replace=True, merge=False),
+	InsertEbuilds(other_overlays["pinsard_overlay"], select=["app-portage/chuse", "x11-wm/qtile"], skip=None, replace=True, merge=False),
         InsertEbuilds(other_overlays["wltjr_overlay"], select=["mail-filter/assp", "mail-mta/netqmail"], skip=None, replace=True, merge=False),
 ]
 
