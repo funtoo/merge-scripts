7a9b9c3f242131de15b5eeb45b3e98801aa319f7
Oleg Vinichenko <oleg@funtoo.org>
2016-10-11 15:16:08 +0000

    update x11 shard

--- funtoo/scripts/merge-funtoo-staging.py
+++ funtoo/scripts/merge-funtoo-staging.py
@@ -34,7 +34,7 @@ shards = {
 	"perl" : GitTree("gentoo-perl-shard", "498dd3a53a832e52aa6c86d97c735bb6f09f134b", "repos@localhost:gentoo-perl-shard.git", pull=True),
 	"kde" : GitTree("gentoo-kde-shard", "17c52fa5cac51d240c2806fac783aaa1a2e9d2da", "repos@localhost:gentoo-kde-shard.git", pull=True),
 	"gnome" : GitTree("gentoo-gnome-shard", "ffabb752f8f4e23a865ffe9caf72f950695e2f26", "repos@localhost:ports/gentoo-gnome-shard.git", pull=True),
-	"x11" : GitTree("gentoo-x11-shard", "b70dd97408765c2b7ee36bdc7ed893516fe481e5", "repos@localhost:ports/gentoo-x11-shard.git", pull=True),
+	"x11" : GitTree("gentoo-x11-shard", "f6868c54cd9ff6c4c72d7276a472b123399bc565", "repos@localhost:ports/gentoo-x11-shard.git", pull=True),
 	"office" : GitTree("gentoo-office-shard", "c5b00a6fba74c79076315e6b398fc3eeb872772c", "repos@localhost:ports/gentoo-office-shard.git", pull=True),
 	"core" : GitTree("gentoo-core-shard", "918411b50c5bddce548ee388ef84276c143fac7a", "repos@localhost:gentoo-core-shard.git", pull=True)
 }
@@ -42,7 +42,7 @@ shards = {
 # perl: 498dd3a53a832e52aa6c86d97c735bb6f09f134b (Updated 5 May 2016)
 # kde: 17c52fa5cac51d240c2806fac783aaa1a2e9d2da  (Updated 9 Oct 2016)
 # gnome: ffabb752f8f4e23a865ffe9caf72f950695e2f26 (Updated 20 Sep 2016)
-# x11: b70dd97408765c2b7ee36bdc7ed893516fe481e5 (Updated 5 Oct 2016)
+# x11: f6868c54cd9ff6c4c72d7276a472b123399bc565 (Updated 11 Oct 2016)
 # office: c5b00a6fba74c79076315e6b398fc3eeb872772c (Updated 8 Jul 2016)
 # core:	918411b50c5bddce548ee388ef84276c143fac7a (Updated 22 Sep 2016)
 # funtoo-toolchain: de2e4d3713568115af81b265c41b64cc4f927b31 (Updated 31 Jul 2016)
