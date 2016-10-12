635f538285098d16fd61600a23f7ad4e7ce0c781
Oleg Vinichenko <oleg@funtoo.org>
2016-10-12 13:58:54 +0000

    update core shard. should fix FL-3345

--- funtoo/scripts/merge-funtoo-staging.py
+++ funtoo/scripts/merge-funtoo-staging.py
@@ -36,7 +36,7 @@ shards = {
 	"gnome" : GitTree("gentoo-gnome-shard", "ffabb752f8f4e23a865ffe9caf72f950695e2f26", "repos@localhost:ports/gentoo-gnome-shard.git", pull=True),
 	"x11" : GitTree("gentoo-x11-shard", "f6868c54cd9ff6c4c72d7276a472b123399bc565", "repos@localhost:ports/gentoo-x11-shard.git", pull=True),
 	"office" : GitTree("gentoo-office-shard", "c5b00a6fba74c79076315e6b398fc3eeb872772c", "repos@localhost:ports/gentoo-office-shard.git", pull=True),
-	"core" : GitTree("gentoo-core-shard", "918411b50c5bddce548ee388ef84276c143fac7a", "repos@localhost:gentoo-core-shard.git", pull=True)
+	"core" : GitTree("gentoo-core-shard", "f641c7cc9039dc3618ea333fc12f7d8f992f3f88", "repos@localhost:gentoo-core-shard.git", pull=True)
 }
 
 # perl: 498dd3a53a832e52aa6c86d97c735bb6f09f134b (Updated 5 May 2016)
@@ -44,7 +44,7 @@ shards = {
 # gnome: ffabb752f8f4e23a865ffe9caf72f950695e2f26 (Updated 20 Sep 2016)
 # x11: f6868c54cd9ff6c4c72d7276a472b123399bc565 (Updated 11 Oct 2016)
 # office: c5b00a6fba74c79076315e6b398fc3eeb872772c (Updated 8 Jul 2016)
-# core:	918411b50c5bddce548ee388ef84276c143fac7a (Updated 22 Sep 2016)
+# core:	f641c7cc9039dc3618ea333fc12f7d8f992f3f88 (Updated 12 Oct 2016)
 # funtoo-toolchain: de2e4d3713568115af81b265c41b64cc4f927b31 (Updated 31 Jul 2016)
 
 funtoo_overlays = {
