03ddee537d189772052fc2dca3a7aa69c442e3b9
Oleg Vinichenko <oleg@funtoo.org>
2016-12-11 18:56:35 +0000

    remove whitespce

--- funtoo/scripts/merge-funtoo-staging.py
+++ funtoo/scripts/merge-funtoo-staging.py
@@ -44,7 +44,7 @@ shards = {
 # gnome: ffabb752f8f4e23a865ffe9caf72f950695e2f26 (Updated 20 Sep 2016)
 # x11: e6896571efc463a4b90b1896f2b5d1810eadd8f7 (Updated 11 Dec 2016)
 # office: 9a702057d23e7fa277e9626344671a82ce59442f (Updated 29 Nov 2016)
-# core:	6d4cc1346d67046cba4e3c39806b7355977fd596 (Updated 11 Dec 2016)
+# core: 6d4cc1346d67046cba4e3c39806b7355977fd596 (Updated 11 Dec 2016)
 # funtoo-toolchain: e1dc33b5b2669bf3af00caa6ce6741503c38cb93 (Updated 26 Nov 2016)
 
 funtoo_overlays = {
