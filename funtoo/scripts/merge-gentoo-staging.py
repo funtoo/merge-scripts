f86fcada65165e1e3fbd2a01d5a097e910fe01a2
Daniel Robbins <drobbins@funtoo.org>
2017-04-15 00:19:52 -0600

    package sets updates and disable shard updates

--- funtoo/scripts/merge-gentoo-staging.py
+++ funtoo/scripts/merge-gentoo-staging.py
@@ -8,17 +8,17 @@ gentoo_staging_w = GitTree("gentoo-staging", "master", "repos@localhost:ports/ge
 # shards are overlays where we collect gentoo's most recent changes. This way, we can merge specific versions rather than always be forced to
 # get the latest.
 
-shard_names = [ "perl", "python", "kde", "gnome", "core", "office", "media" ]
-shards = {}
-shard_steps = {}
+#shard_names = [ "perl", "python", "kde", "gnome", "core", "office", "media" ]
+#shards = {}
+#shard_steps = {}
 
-for s in shard_names:
-	shard_steps[s] = []
-	if s.endswith("-kit"):
-		shards[s] = GitTree(s, "master", "repos@localhost:kits/%s.git" % s, root="/var/git/dest-trees/%s" % s, pull=False)
-	else:
-		shards[s] = GitTree("gentoo-%s-shard" % s, "master", "repos@localhost:ports/gentoo-%s-shard.git" % s, root="/var/git/dest-trees/gentoo-%s-shard" % s, pull=False)
-	shard_steps[s] += generateShardSteps(s, gentoo_staging_w, shards[s])
+#for s in shard_names:
+#	shard_steps[s] = []
+#	if s.endswith("-kit"):
+#		shards[s] = GitTree(s, "master", "repos@localhost:kits/%s.git" % s, root="/var/git/dest-trees/%s" % s, pull=False)
+#	else:
+#		shards[s] = GitTree("gentoo-%s-shard" % s, "master", "repos@localhost:ports/gentoo-%s-shard.git" % s, root="/var/git/dest-trees/gentoo-%s-shard" % s, pull=False)
+#	shard_steps[s] += generateShardSteps(s, gentoo_staging_w, shards[s])
 
 # This function updates the gentoo-staging tree with all the latest gentoo updates:
 
@@ -44,9 +44,9 @@ def gentoo_staging_update():
 	gentoo_staging_w.run(all_steps)
 	gentoo_staging_w.gitCommit(message="gentoo updates", branch="master")
 
-	for s in shard_names:
-		shards[s].run(shard_steps[s])
-		shards[s].gitCommit(message="gentoo updates", branch="master")
+#	for s in shard_names:
+#		shards[s].run(shard_steps[s])
+#		shards[s].gitCommit(message="gentoo updates", branch="master")
 
 gentoo_staging_update()
 
