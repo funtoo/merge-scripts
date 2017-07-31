23a65e471156e185a04d1dbf597e16a476455e44
Daniel Robbins <drobbins@funtoo.org>
2017-07-30 21:52:03 -0600

    Turn off ports-2012 updates

--- funtoo/scripts/master-cron.sh
+++ funtoo/scripts/master-cron.sh
@@ -2,4 +2,5 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-$DIR/merge-gentoo-staging.py && $DIR/merge-funtoo-staging.py && $DIR/merge-funtoo-production.py
+$DIR/merge-gentoo-staging.py 
+#&& $DIR/merge-funtoo-staging.py && $DIR/merge-funtoo-production.py
