ba129bc4bc16524155247bebac9baae07d72b8be
Daniel Robbins <drobbins@funtoo.org>
2018-01-01 21:51:21 -0700

    Adding current gentoo-staging as the LAST kit in the kit_source_defs for a lot of kits, after the snapshot of gentoo-staging. This will allow the merge scripts to grab new ebuilds added to Gentoo after the snap date, so they don't end up in nokit. For example, this should move all of dev-python to python-kits whereas now newly-added dev-python catpkgs are ending up in nokit, which is messy.

--- funtoo/scripts/2017/merge-all-kits.py
+++ funtoo/scripts/2017/merge-all-kits.py
@@ -177,6 +177,8 @@ kit_source_defs = {
 		{ "repo" : "fusion809", "src_sha1" : "489b46557d306e93e6dc58c11e7c1da52abd34b0", 'date' : '31 Aug 2017' },
 		{ "repo" : "rh1", },
 		{ "repo" : "gentoo-staging", "src_sha1" : '80d2f3782e7f351855664919d679e94a95793a06', 'date' : '31 Aug 2017'},
+		# add current gentoo-staging to catch any new ebuilds that are not yet in our snapshot above (dev-foo/* match)
+		{ "repo" : "gentoo-staging" },
 	],
 	"funtoo_mk3_prime" : [
 		# allow overlays to override gentoo
@@ -185,6 +187,7 @@ kit_source_defs = {
 		{ "repo" : "fusion809", "src_sha1" : "8733034816d3932486cb593db2dfbfbc7577e28b", 'date' : '09 Oct 2017' },
 		{ "repo" : "rh1", },
 		{ "repo" : "gentoo-staging", "src_sha1" : '2de4b388863ab0dbbd291422aa556c9de646f1ff', 'date' : '10 Oct 2017'},
+		{ "repo" : "gentoo-staging" },
 	],
 	"funtoo_mk3_late_prime": [
 		# allow overlays to override gentoo
@@ -193,6 +196,7 @@ kit_source_defs = {
 		{"repo": "fusion809", "src_sha1": "574f9f6f69b30f4eec7aa2eb53f55059d3c05b6a", 'date': '23 Oct 2017'},
 		{"repo": "rh1", },
 		{"repo": "gentoo-staging", "src_sha1": 'aa03020139bc129af2ad5f454640c102afa712e6', 'date': '22 Oct 2017'},
+		{"repo": "gentoo-staging" },
 	],
 	"funtoo_prime" : [
 		# allow overlays to override gentoo
@@ -201,6 +205,7 @@ kit_source_defs = {
 		{ "repo" : "fusion809", "src_sha1" : "8322bcd79d47ef81f7417c324a1a2b4772020985" },
 		{ "repo" : "rh1", },
 		{ "repo" : "gentoo-staging", "src_sha1" : '06a1fd99a3ce1dd33724e11ae9f81c5d0364985e', 'date' : '21 Apr 2017'},
+		{ "repo" : "gentoo-staging" },
 	],
 	"gentoo_prime_mk3_protected" : [
 		# lock down core-kit and security-kit
