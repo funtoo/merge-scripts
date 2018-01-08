398b74a3e1b57b3dddf0f1b20dfee8977859d460
Oleg Vinichenko <oleg@funtoo.org>
2018-01-08 14:15:42 +0000

    set alpha stability to security-kit and lock down core-kit and security-kit

--- funtoo/scripts/2017/merge-all-kits.py
+++ funtoo/scripts/2017/merge-all-kits.py
@@ -213,6 +213,10 @@ kit_source_defs = {
 		# lock down core-kit and security-kit
 		{ "repo" : "gentoo-staging", "src_sha1" : '2de4b388863ab0dbbd291422aa556c9de646f1ff', 'date' : '10 Oct 2017'},
 	],
+	"gentoo_prime_mk4_protected" : [
+		# lock down core-kit and security-kit
+		{ "repo" : "gentoo-staging", "src_sha1" : '887b32c487432a9206208fc42a313e9e0517bf2b', 'date' : '8 Jan 2018'},
+	],
 	"gentoo_prime_protected" : [
 		# lock down core-kit and security-kit
 		{ "repo" : "gentoo-staging", "src_sha1" : '06a1fd99a3ce1dd33724e11ae9f81c5d0364985e', 'date' : '21 Apr 2017'},
@@ -324,11 +328,11 @@ kit_groups = {
 	'prime' : [
 		{ 'name' : 'core-kit', 'branch' : '1.0-prime', 'source': 'gentoo_prime_protected', 'default' : True },
 		{ 'name' : 'core-kit', 'branch' : '1.1-prime', 'source': 'gentoo_prime_mk3_protected', 'stability' : KitStabilityRating.DEPRECATED },
-		{ 'name' : 'core-kit', 'branch': '1.2-prime', 'source': 'funtoo_current', 'stability': KitStabilityRating.ALPHA},
+		{ 'name' : 'core-kit', 'branch': '1.2-prime', 'source': 'gentoo_prime_mk4_protected', 'stability': KitStabilityRating.ALPHA},
 		{ 'name' : 'core-hw-kit', 'branch' : 'master', 'source': 'funtoo_current', 'default' : True },
 		{ 'name' : 'security-kit', 'branch' : '1.0-prime', 'source': 'gentoo_prime_protected', 'default' : True },
 		{ 'name' : 'security-kit', 'branch' : '1.1-prime', 'source': 'gentoo_prime_mk3_protected', 'stability' : KitStabilityRating.DEPRECATED },
-		{ 'name' : 'security-kit', 'branch': '1.2-prime', 'source': 'funtoo_current', 'stability': KitStabilityRating.DEV},
+		{ 'name' : 'security-kit', 'branch': '1.2-prime', 'source': 'gentoo_prime_mk4_protected', 'stability': KitStabilityRating.ALPHA},
 		{ 'name' : 'xorg-kit', 'branch' : '1.17-prime', 'source': 'funtoo_prime_xorg', 'default' : False, 'stability' : KitStabilityRating.PRIME },
 		{ 'name' : 'xorg-kit', 'branch' : '1.19-prime', 'source': 'funtoo_mk2_prime', 'default' : True, 'stability' : KitStabilityRating.PRIME  }, # MK2
 		{ 'name' : 'gnome-kit', 'branch' : '3.20-prime', 'source': 'funtoo_prime_gnome', 'default' : True },
